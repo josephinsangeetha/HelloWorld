1a597dcfb4f1aea80cdc1f884d65f4cb60a5fdb9
Admin <Admin@192.168.108.1>
2019-05-17 08:41:10 +0530

    Fourth commit

---
+++ Hello World/src/ArrayElements.java
@@ -0,0 +1,14 @@
+
+public class ArrayElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[] = {12, 34};
+int sum = 0;
+for (int num : arr) {
+	sum = sum+num;
+}
+System.out.println("Sum of array elements is:"+sum);
+	}
+
+}
