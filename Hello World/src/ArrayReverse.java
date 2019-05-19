b3e37c52aff8bb4032daf38801d792014f146b58
Admin <Admin@192.168.108.1>
2019-05-19 14:48:11 +0530

    Array Reverse

--- Hello World/src/ArrayReverse.java
+++ Hello World/src/ArrayReverse.java
@@ -1,49 +1,30 @@
-import java.util.Scanner;
-import java.util.Stack;
+
 
 public class ArrayReverse {
-
-	int counter, i=0, j=0, temp;
-	int number[] = new int[100];
-	Scanner scanner = new Scanner(System.in);
-	System.out.print("How many elements you want to enter: ");
-	counter = scanner.nextInt();
-		
-	/* This loop stores all the elements that we enter in an 
-	 * the array number. First element is at number[0], second at 
-	 * number[1] and so on
-	 */
-	for(i=0; i<counter; i++)
-	{
-	    System.out.print("Enter Array Element"+(i+1)+": ");
-	    number[i] = scanner.nextInt();
-	}
-
-	/* Here we are writing the logic to swap first element with
-	 * last element, second last element with second element and
-	 * so on. On the first iteration of while loop i is the index 
-	 * of first element and j is the index of last. On the second
-	 * iteration i is the index of second and j is the index of 
-	 * second last.
-	 */
-	j = i - 1;     
-	i = 0;         
-	scanner.close();
-	while(i<j)
-	{
-  	   temp = number[i];
-	   number[i] = number[j];
-	   number[j] = temp;
-	   i++;
-	   j--;
-	}
-
-	System.out.print("Reversed array: ");
-	for(i=0; i<counter; i++)
-	{
-	   System.out.print(number[i]+ "  ");
-	}   
+	
+		static void reverse(int a[], int n) 
+	    { 
+	        int[] b = new int[n]; 
+	        int j = n; 
+	        for (int i = 0; i < n; i++) { 
+	            b[j - 1] = a[i]; 
+	            j = j - 1; 
+	        } 
+	 
+	        /*printing the reversed array*/
+	        System.out.println("Reversed array is: \n");
+	        for (int k = 0; k < n; k++) { 
+	            System.out.println(b[k]); 
+	        } 
+	    } 
+	  
+	    public static void main(String[] args) 
+	    { 
+	        int [] arr = {10, 20, 30, 40, 50}; 
+	        reverse(arr, arr.length); 
+	    } 
+	
 }
-}
+
 
 
