597cf393a149c83b5fcfa19fdeda47fa0fb9fa32
Admin <Admin@192.168.108.1>
2019-05-19 22:06:44 +0530

    Char array

---
+++ Hello World/src/Chararraytostring.java
@@ -0,0 +1,12 @@
+
+public class Chararraytostring {
+
+	public static void main(String[] args) {
+	 char [] a = {'J','o','s','e','p','h','i','n',' ','C','J','S','R'};
+			 String str = new String(a);
+	 
+	System.out.println("Character to string:" + str);
+ 
+	}
+}
+
